c00b66a37d24d1af8c34c83f2626d998cb9fdbfa
w32807 <w32807@nate.com>
2021-05-03 21:04:05 +0900

    add Junit Test

--- src/main/java/com/toby/spring/UserDaoTest.java
+++ src/main/java/com/toby/spring/UserDaoTest.java
@@ -1,62 +1,59 @@
  package com.toby.spring;
 
-import java.sql.SQLException;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import javax.sql.DataSource;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.toby.spring.dao.UserDao;
 import com.toby.spring.domain.User;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/applicationContext.xml"})
+@DirtiesContext // 원래 설정한 스프링의 설정이 아닌, 테스트 코드에서 컨텍스트 설정이 바뀔 것을 알려주는 어노테이
 public class UserDaoTest {
-    // JUnit의 Test 코드로 전환
-    /*
-    public static void main(String[] args) {
-        // 스프링 Legacy 프로젝트가 아닐 때 ApplicationContext 사용하기
-        // ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        // 등록된 Bean의 이름, 클래스 를 매개변수로 Bean을 얻어온다.
-        
-        // XML 설정파일일 경우
-        // 왜인지 아직 모르겠지만, UserDao의 주입을 생성자가 아닌 setter 방식으로 변경 해야 오류가 안남.
-        ApplicationContext context = new GenericXmlApplicationContext("/applicationContext.xml");
-        
-        UserDao dao1 = context.getBean("userDao", UserDao.class);
-        UserDao dao2 = context.getBean("userDao", UserDao.class);
-        
-        System.out.println(dao1 == dao2);
-        System.out.println(dao1.equals(dao2));
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        
-        // connection을 만드는 역할을, 클라이언트가 아닌 팩토리로 넘겨버림
-        UserDao dao = new DaoFactory().userDao();
-        System.out.println(dao);
-    }
-    */
     
+    @Autowired
+    private UserDao dao;
+    
+    @Before
+    public void setUp() {
+        // 혹시 모르니, 운영 DB가 아닌 테스트 DB로 명시적인 연결을 해준다.
+        // 근데 이 방법을 쓰려면, 꼭 dao는 datasource를 생성자로써 주입 받아야 하나?
+        // 그러나 이 방법보다, test-applicationContext.xml 파일을 만드는 것이 더 깔끔.
+        DataSource dataSource = new SingleConnectionDataSource(
+            "jdbc:mysql://localhost/tobySpring?serverTimezone=Asia/Seoul"
+                ,"root", "1234", true);
+        dao.setDataSource(dataSource);
+    }
     @Test
-    public void insertTest() {
-        ApplicationContext context = new GenericXmlApplicationContext("/applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
+    public void insertTest() throws Exception{
+        dao.deleteAll();
         
         User user = new User();
-        
         user.setId("user");
         user.setName("원준");
         user.setPassword("1234");
-        try {
-            dao.add(user);
-            User user2 = dao.get(user.getId());
-            System.out.println(user2.getId());
-        } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-        
+        dao.add(user);
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getId());
         
     }
+    
+    // 테스트 도중, 발생할 것으로 기대하는 예외클래스를 지정함
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void getUserFailure() throws Exception{
+        dao.deleteAll();
+        assertEquals(dao.getCount(), 0);
+        dao.get("unknown_id");
+    }
+    
 }
--- src/main/java/com/toby/spring/dao/UserDao.java
+++ src/main/java/com/toby/spring/dao/UserDao.java
@@ -7,6 +7,9 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.junit.Test;
+import org.springframework.dao.EmptyResultDataAccessException;
+
 import com.toby.spring.domain.User;
 
 public class UserDao {
@@ -45,21 +48,45 @@ public class UserDao {
         //Connection c = connectionMaker.makeConnection();
         Connection c = dataSource.getConnection();
         
-        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        PreparedStatement ps = c.prepareStatement(" select * from users where id = ? ");
         ps.setString(1, id);
         
         ResultSet rs = ps.executeQuery();
-        rs.next();
         
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
+        User user = null;
+        if(rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
         
         rs.close();
         ps.close();
         c.close();
-        
+        if(user == null) throw new EmptyResultDataAccessException(1); 
         return user;
+    }
+    
+    public void deleteAll() throws SQLException{
+        Connection c = dataSource.getConnection();
+        
+        PreparedStatement ps = c.prepareStatement("delete from users");
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+    }
+    
+    public int getCount() throws SQLException{
+        Connection c = dataSource.getConnection();
+        PreparedStatement ps = c.prepareStatement("select Count(*) from users");
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+        rs.close();
+        ps.close();
+        c.close();
+        
+        return count;
     }
 }
